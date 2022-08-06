a2a7c55384378e78f8808473b41774b278a70d16
Surya2742 <boby7724284@gmail.com>
2022-08-06 08:46:28 +0530

    added: Game starts at 0 position

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,6 +1,6 @@
 public class SnakeLadder {
     public static void main(String[] args) {
         int startPosition = 0;
-        System.out.println("Game starts and Player is at Position " + startPosition);
+        System.out.println("Game starts and Player is at Position : " + startPosition);
     }
 }
