3c8a517df38593bdc004a61a9816db1a797dd5b1
Surya2742 <boby7724284@gmail.com>
2022-08-07 19:38:11 +0530

    added: Rolling die value from one to six

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -2,5 +2,7 @@ public class SnakeLadder {
     public static void main(String[] args) {
         int startPosition = 0;
         System.out.println("Game starts and Player is at Position : " + startPosition);
+        int dice = (int) Math.floor(Math.random() * 6 + 1);
+        System.out.println("The Number on dice is " + dice);
     }
 }
