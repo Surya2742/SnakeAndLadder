5c454a5a3767bd35964a0ecf26b98df362ba00d3
Surya2742 <boby7724284@gmail.com>
2022-08-07 23:14:37 +0530

    added: Game has Two Player and Game finds the Winner

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,39 +1,59 @@
 public class SnakeLadder {
     public static void main(String[] args) {
-        int playerCurrentPosition = 0;
+        int playerOneCurrentPosition = 0;
+        int playerTwoCurrentPosition = 0;
         final int noPlay = 0;
         final int ladder = 1;
         final int snake = 2;
         int dieCounter = 0;
-        System.out.println("Game starts and Player is at Position : " + playerCurrentPosition + "\n");
-        while(playerCurrentPosition < 100) {
+        System.out.println("Game starts:\nPlayer One is at Position : " + playerOneCurrentPosition + "\nPlayer Two is at Position : " + playerTwoCurrentPosition + "\n");
+        while(playerOneCurrentPosition < 100 && playerTwoCurrentPosition < 100) {
             dieCounter++;
-            int dice = (int) Math.floor(Math.random() * 6 + 1);
-            System.out.println("The Number on dice is " + dice);
+            int playerOneDice = (int) Math.floor(Math.random() * 6 + 1);
+            System.out.println("The Number on Player One dice is " + playerOneDice);
+            int playerTwoDice = (int) Math.floor(Math.random() * 6 + 1);
+            System.out.println("The Number on Player Two dice is " + playerTwoDice);
             int playerAction = (int) (Math.random() * 3);
             if (playerAction == ladder) {
                 System.out.println("Option: Ladder");
-                playerCurrentPosition += dice;
-                if(playerCurrentPosition > 100) {
-                    System.out.println("Player cannot go above 100");
-                    playerCurrentPosition -= dice;
+                playerOneCurrentPosition += playerOneDice;
+                playerTwoCurrentPosition += playerTwoDice;
+                if(playerOneCurrentPosition > 100) {
+                    System.out.println("Player One cannot go above 100");
+                    playerOneCurrentPosition -= playerOneDice;
                 }
-                System.out.println("Ladder moves the player at " + playerCurrentPosition + "\n");
+                System.out.println("Ladder moves the Player One at " + playerOneCurrentPosition);
+                if(playerTwoCurrentPosition > 100) {
+                    System.out.println("Player Two cannot go above 100");
+                    playerTwoCurrentPosition -= playerTwoDice;
+                }
+                System.out.println("Ladder moves the Player Two at " + playerTwoCurrentPosition + "\n");
             }
             else if (playerAction == snake) {
                 System.out.println("Option: Snake");
-                playerCurrentPosition -= dice;
-                if(playerCurrentPosition < 0) {
-                    System.out.println("Player cannot go below 0");
-                    playerCurrentPosition = 0;
+                playerOneCurrentPosition -= playerOneDice;
+                playerTwoCurrentPosition -= playerTwoDice;
+                if(playerOneCurrentPosition < 0) {
+                    System.out.println("Player One cannot go below 0");
+                    playerOneCurrentPosition = 0;
                 }
-                System.out.println("Snake moves the player at " + playerCurrentPosition + "\n");
+                System.out.println("Snake moves the Player One at " + playerOneCurrentPosition);
+                if(playerTwoCurrentPosition < 0) {
+                    System.out.println("Player Two cannot go below 0");
+                    playerTwoCurrentPosition = 0;
+                }
+                System.out.println("Snake moves the Player Two at " + playerTwoCurrentPosition + "\n");
             }
             else if (playerAction == noPlay) {
                 System.out.println("Option: No Play");
-                System.out.println("Player stays on " + playerCurrentPosition + "\n");
+                System.out.println("Player One stays at " + playerOneCurrentPosition);
+                System.out.println("Player Two stays at " + playerTwoCurrentPosition + "\n");
             }
         }
         System.out.println("Die has been rolled for " + dieCounter + " times.");
+        if(playerOneCurrentPosition == 100)
+            System.out.println("Player One Wins the Game");
+        else  if(playerTwoCurrentPosition == 100)
+            System.out.println("Player Two Wins the Game");
     }
 }
